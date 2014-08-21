56c2b6227df18dffb2eaf7fa5a0492f83f13ac77
Katharine Chen & Sean Morrison <student@epicodus.com>
2014-08-21 12:15:36 -0700

    add new chapter (create).

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -7,4 +7,15 @@ class ChaptersController < ApplicationController
   def new
     render("new.html.erb")
   end
+
+  def create
+    @chapter = Chapter.new(params[:chapter])
+    if @chapter.save(params[:chapter])
+      render('chapters/success.html.erb')
+    else
+      render('chapters/new.html.erb')
+    end
+  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   match('/', {:via => :get, :to => 'chapters#index'})
   match('chapters', {:via => :get, :to => 'chapters#index'})
   match('chapters/new', {:via => :get, :to => 'chapters#new'})
+  match('chapters', {:via => :post, :to => 'chapters#create'})
 end
